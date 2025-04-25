3ecd6db842b999191b7aaf1612cc090d3811f955
matiuuh <estuardonoriega36@gmail.com>
2025-04-25 15:55:00 -0600

    Se elimino archivo sin usar

--- Client/src/components/Consola.jsx
+++
@@ -1,8 +0,0 @@
-export default function Consola({ texto }) {
-    return (
-        <div className="seccion">
-            <h3>Consola</h3>
-            <pre className="consola">{texto}</pre>
-        </div>
-    );
-}
